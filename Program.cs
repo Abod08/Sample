3755ef4af23bc02690a0c9cb57e2609e1562fc82
Abod08 <ayindeabodunrin@gmail.com>
2023-12-07 10:30:47 +0100

    Social Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linqq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> nums = new List<int> { 4, -2, 10, -7, 15, 60, 25, 31 };
+
+            var positiveNumber = nums.Where(x => x > 0 && x < 20 && x % 2 == 0);
+
+            foreach (var num in positiveNumber)
+            {
+                System.Console.WriteLine(num);
+            }
+
+            var Query = (from num in nums
+                        where num > 0 && num < 20 && num % 2 == 0
+                        select num).ToList();
+                        
+            foreach (var num in Query)
+            {
+                System.Console.WriteLine(num);
+            }
+
+        }
+    }
+}
